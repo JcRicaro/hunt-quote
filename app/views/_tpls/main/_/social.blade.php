3f931b32297318d52f04681f52cb3648e075e6c2
srph <seraphipod@gmail.com>
2014-10-23 08:02:37 +0800

    (upadte)

--- app/views/_tpls/main/_/social.blade.php
+++ app/views/_tpls/main/_/social.blade.php
@@ -1,4 +1,4 @@
-<div class="row">
+<!-- <div class="row">
 	<div class="col-md-2" style="width: 140px;">
 		<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
 	</div>
@@ -6,4 +6,12 @@
 	<div class="col-md-2" style="width: 50px;">
 		<a class="twitter-share-button" href="https://twitter.com/share"> Tweet </a>
 	</div>
+</div> -->
+
+<div class="pw-widget pw-counter-none">
+	<a class="pw-button-facebook pw-look-native"></a>
+	<a class="pw-button-pinterest pw-look-native"></a>
+	<a class="pw-button-twitter pw-look-native"></a>
+	<a class="pw-button-post-share"></a>
 </div>
+<script src="http://i.po.st/static/v3/post-widget.js#publisherKey=[publisherKey]&retina=true" type="text/javascript"></script>
