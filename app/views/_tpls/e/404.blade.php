2c8b7d2c31b53bb603062566731d63e37c1acb31
srph <seraphipod@gmail.com>
2014-10-23 12:41:03 +0800

    added 404

---
+++ app/views/_tpls/e/404.blade.php
@@ -0,0 +1,9 @@
+@extends('_tpls.main.ads')
+
+@section('title') 404 @stop
+
+@section('sub-content')
+	<h1> 404 </h1>
+	<h4> An error has occured; either the page or specified author does not exist! </h4> 
+	<hr>
+@stop
