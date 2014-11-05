76bdecc600a54d9ca8714c738145be8525f8e490
srph <seraphipod@gmail.com>
2014-11-05 12:07:39 +0800

    ..

--- app/views/dashboard/tags/index.blade.php
+++ app/views/dashboard/tags/index.blade.php
@@ -16,6 +16,58 @@
 	</ol>
 @stop
 
+@section('content')
+	<div class="box box-solid">
+		<div class="box-header">
+			<h3 class="box-title">
+				Tag List
+			</h3>
+			<div class="box-tools pull-right">
+				{{ $data->links() }}
+			</div>
+		</div>
+		<div class="box-body">
+			@include('_tpls.dashboard._.flash')
+			<a href="{{ URL::to('dashboard/tags/create') }}" class="btn pull-right">
+				<i class="fa fa-plus"></i> Create
+			</a>
+			
+			<div class="clearfix"></div>
+
+			<table class="table">
+				<thead>
+					<tr>
+						<th>
+							Tag
+						</th>
+						<th class="col-sm-2">
+							
+						</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($data as $tag)
+					<tr>
+						<td>
+							{{ $tag->name }}
+						</td>
+						<td class="text-right">
+							<a href="{{ URL::to('dashboard/tags/' . $tag->id . '/edit') }}" class="btn">
+								<i class="fa fa-edit"></i>
+							</a>
+							<a href="#" class="delete btn" data-id="{{ $tag->id }}">
+								<i class="fa fa-remove"></i>
+							</a>
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+		</div>
+	</div>
+@stop
+
+
 @section('scripts')
 	{{ HTML::script('assets/admin-lte/js/plugins/bootbox/bootbox.min.js') }}
 
